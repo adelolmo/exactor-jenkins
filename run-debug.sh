91ff36889c25824bed906fd16f0138e1ea5ffc2b
adelolmo <andoni_del.olmo_elizari@smeet.com>
2015-04-30 14:21:10 +0200

    add readme image.

--- run-debug.sh
+++ run-debug.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-$M2_HOME/bin/mvnDebug clean package -DskipTests -Djetty.port=8080 hpi:run
+echo $M2_HOME
+usage (){
+    echo "Please define M2_HOME environment valiable."; exit 1
+}
+
+[ -z "$M2_HOME" ] &&  usage || echo $M2_HOME
+echo 'Starting Jenkins debug'
+$M2_HOME/bin/mvnDebug clean package -DskipTests -Djetty.port=8089 hpi:run
